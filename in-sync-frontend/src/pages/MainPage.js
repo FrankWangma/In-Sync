1ce29425cb15b3f39e23e6bb874e1144e2955491
Frank Wang Ma <frankwangma@gmail.com>
2020-04-10 19:40:39 +1200

    Added initial Main Page components

--- in-sync-frontend/src/pages/MainPage.js
+++ in-sync-frontend/src/pages/MainPage.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Typography, Grid } from '@material-ui/core';
+
+import CreateRoomButton from '../components/CreateRoomButton'
+
 
 class MainPage extends React.Component {
     render() {
         return (
             <div className="App">
                 <header className="App-header">
-                    <p>Main Page Placeholder</p>
+                    <Grid container spacing={0}>
+                        <Grid item xs={12}>
+                            <Typography variant='h1'>
+                                In-Sync
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <CreateRoomButton />
+                        </Grid>
+                    </Grid>
                 </header>
             </div>
         );

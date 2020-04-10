559d2aca01324a8559400829f525095c94243aa1
Hayden <hdnsimpson@gmail.com>
2020-04-10 14:32:32 +1200

    Added placeholder video/room page

---
+++ in-sync-frontend/src/pages/VideoPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class VideoPage extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>Video Page Placeholder</p>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default MainPage;

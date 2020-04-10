8f3022eb2ac23d703b40bf0de0644a3dfab6d88f
Hayden <hdnsimpson@gmail.com>
2020-04-10 14:16:36 +1200

    Created basic components and pages structure

---
+++ in-sync-frontend/src/components/EmbeddedVideo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class EmbeddedVideo extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>Embedded Video Placeholder</p>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default EmbeddedVideo;
---
+++ in-sync-frontend/src/pages/MainPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>Main Page Placeholder</p>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default MainPage;

47bd4acc5057f1e1fdd23acc79f399f486ccf162
Frank Wang Ma <frankwangma@gmail.com>
2020-04-10 14:58:31 +1200

    Added React Router and routing to different pages

---
+++ in-sync-frontend/src/routes/routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import MainPage from '../Pages/MainPage';
+
+const paths = {
+    MAINPAGE: '/',
+}
+
+const routes = (
+    <Switch>
+        <Route exact path={paths.MAINPAGE} component={MainPage} />
+    </Switch>
+)
+
+export default routes

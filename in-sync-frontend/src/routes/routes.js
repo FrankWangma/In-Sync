6ca131d50f5f703dd55f0d0f09cf713f00342772
Hayden <hdnsimpson@gmail.com>
2020-04-13 14:27:16 +1200

    Fixed package merge conflict, added temp video page route

--- in-sync-frontend/src/routes/routes.js
+++ in-sync-frontend/src/routes/routes.js
@@ -2,14 +2,17 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import MainPage from '../pages/MainPage';
+import VideoPage from '../pages/VideoPage';
 
 const paths = {
     MAINPAGE: '/',
+    VIDEOPAGE: '/video'
 }
 
 const routes = (
     <Switch>
         <Route exact path={paths.MAINPAGE} component={MainPage} />
+        <Route exact path={paths.VIDEOPAGE} component={VideoPage} />
     </Switch>
 )
 

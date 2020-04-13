0fdec9e4a673a845170326a1442e0843ba8e95ef
Frank Wang Ma <frankwangma@gmail.com>
2020-04-13 18:45:36 +1200

    Added initial Chat Component with dummy data

---
+++ in-sync-frontend/src/components/ChatComponent.js
@@ -0,0 +1,41 @@
+import React from "react";
+import 'react-chatbox-component/dist/style.css';
+import { ChatBox } from 'react-chatbox-component';
+
+const messages = [
+  {
+    "text": "Hello there",
+    "id": "1",
+    "sender": {
+      "name": "Ironman",
+      "uid": "user1",
+      "avatar": "https://data.cometchat.com/assets/images/avatars/ironman.png",
+    },
+  },
+  {
+    "text": "Hi",
+    "id": "2",
+    "sender": {
+      "name": "Spiderman",
+      "uid": "user2",
+      "avatar": "https://data.cometchat.com/assets/images/avatars/spiderman.png",
+    },
+  },
+]
+const user = {
+  "uid" : "user1"
+}
+class ChatComponent extends React.Component {
+    render() {
+        return (
+          <div className='container'>
+            <div className='chat-header'>
+              <h5>Chat</h5>
+            </div>
+            <ChatBox messages={messages} user={user}/>
+        </div>
+        );
+      }
+}
+
+export default ChatComponent;
--- in-sync-frontend/src/pages/VideoPage.js
+++ in-sync-frontend/src/pages/VideoPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import EmbeddedVideo from "../components/EmbeddedVideo";
+import ChatComponent from "../components/ChatComponent";
 
 class VideoPage extends React.Component {
   render() {
@@ -8,6 +9,7 @@ class VideoPage extends React.Component {
         <header className="App-header">
           <p>Video Page Placeholder</p>
           <EmbeddedVideo url='https://www.youtube.com/watch?v=dQw4w9WgXcQ' />
+          <ChatComponent className="chat"/>
         </header>
       </div>
     );

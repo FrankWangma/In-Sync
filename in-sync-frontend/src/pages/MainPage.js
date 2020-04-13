127e92c6129759d356d8a7e10edd1c94af4bd040
Hayden <hdnsimpson@gmail.com>
2020-04-13 15:05:36 +1200

    Addressed lint test

--- in-sync-frontend/src/pages/MainPage.js
+++ in-sync-frontend/src/pages/MainPage.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
 class MainPage extends React.Component {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <p>Main Page Placeholder</p>
-                </header>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Main Page Placeholder</p>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default MainPage;

369b42fd790862dfc8b91a1174ec7f7273bad521
Frank <38555691+FrankWangma@users.noreply.github.com>
2020-04-13 17:20:04 +1200

    Feature main page (#31)
    * Added initial Main Page components
    
    * Changed the name of the create and join room component to more appropriately represent the component
    
    * Fixed an issue with Title being too far up
    
    * Ran the lint and the fix-lint commands
    
    * Added basic css to the buttons
    
    * Added changes based on PR review

---
+++ in-sync-frontend/src/components/CreateJoinRooms.js
@@ -0,0 +1,42 @@
+import React from "react";
+import "./CreateJoinRooms.css";
+import { Button, Grid, Typography } from "@material-ui/core";
+
+class CreateJoinRoomButton extends React.Component {
+  render() {
+    return (
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <Button variant="contained" className={"button"}>
+                        <Grid item xs={6}>
+                            <Typography variant={"h5"}>
+                                Join Room
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Typography variant={"subtitle1"}>
+                                Join an existing In-Sync room
+                            </Typography>
+                        </Grid>
+                    </Button>
+                </Grid>
+                <Grid item xs={12}>
+                    <Button variant="contained" className={"button"}>
+                        <Grid item xs={6}>
+                            <Typography variant={"h5"}>
+                                Create Room
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <Typography variant={"subtitle1"}>
+                                Create a new In-Sync room
+                            </Typography>
+                        </Grid>
+                    </Button>
+                </Grid>
+            </Grid>
+    );
+  }
+}
+
+export default CreateJoinRoomButton;

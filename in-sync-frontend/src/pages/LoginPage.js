34aa63584f153903413c46305788b0d713bc3401
Frank Wang Ma <frankwangma@gmail.com>
2020-04-20 20:53:05 +1200

    Added Grid to login page to put sign-up and login component side by side

--- in-sync-frontend/src/pages/LoginPage.js
+++ in-sync-frontend/src/pages/LoginPage.js
@@ -2,14 +2,23 @@ import React from "react";
 import Login from "../components/LoginComponent"
 import SignUp from "../components/SignUpComponent"
 import styles from "./LoginPage.module.css";
+import { Grid } from "@material-ui/core";
 
 const LoginPage = () => {
     return (
         <div className={styles.LoginPage}>
-            <Login />
-            <SignUp />
+            <Grid container spacing={5}>
+                <Grid item xs={2} />
+                <Grid item sm={12} md={4}>
+                    <Login />
+                </Grid>
+                <Grid item sm={12} md={4}>
+                    <SignUp />
+                </Grid>
+                <Grid item xs={2} />
+            </Grid>
         </div>
     );
 }
 
-export default LoginPage;
+export default LoginPage; 

eaa19e74e1c1ad8f19f34d07d6102df11b55f9e1
Hayden <hdnsimpson@gmail.com>
2020-04-22 16:13:00 +1200

    Added user dummy data

--- in-sync-frontend/src/components/UserList.js
+++ in-sync-frontend/src/components/UserList.js
@@ -2,12 +2,26 @@ import React from "react";
 import 'react-chatbox-component/dist/style.css';
 import { ChatBox } from 'react-chatbox-component';
 
+// Users would populate from backend
+let users = [
+  {
+    "name": "Ironman",
+    "uid": "user1",
+    "avatar": "https://data.cometchat.com/assets/images/avatars/ironman.png",
+  },
+  {
+    "name": "Spiderman",
+    "uid": "user2",
+    "avatar": "https://data.cometchat.com/assets/images/avatars/spiderman.png",
+  },
+]
+
 const UserList = () => {
-    return (
-        <div className='container'>
-            <ChatBox />
-        </div>
-    );
+  return (
+    <div className='container'>
+      <ChatBox />
+    </div>
+  );
 }
 
 export default UserList;

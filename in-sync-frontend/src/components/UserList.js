b5b7919b52f8695074bdd19eb0695a31292ecc94
Hayden <hdnsimpson@gmail.com>
2020-04-22 16:11:15 +1200

    Changed to functional component

--- in-sync-frontend/src/components/UserList.js
+++ in-sync-frontend/src/components/UserList.js
@@ -2,14 +2,12 @@ import React from "react";
 import 'react-chatbox-component/dist/style.css';
 import { ChatBox } from 'react-chatbox-component';
 
-class UserList extends React.Component {
-    render() {
-        return (
-          <div className='container'>
+const UserList = () => {
+    return (
+        <div className='container'>
             <ChatBox />
         </div>
-        );
-    }
+    );
 }
 
 export default UserList;

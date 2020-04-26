bec0a8d550528271e07da72446cb8e29577c42bb
Hayden <39291167+hdnsimpson@users.noreply.github.com>
2020-04-26 13:19:14 +1200

    Feature functional mainpage (#48)
    * Removed unnecessary backslash, changed mainpage to functional component
    
    * Lint test changes
    
    * Fixed weird indentation

--- in-sync-frontend/src/pages/MainPage.js
+++ in-sync-frontend/src/pages/MainPage.js
@@ -1,34 +1,32 @@
 import React from "react";
-import styles from "./MainPage.module.css";
 import { Button, Typography, Grid } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import styles from "./MainPage.module.css";
 import CreateJoinRoomButton from "../components/CreateJoinRooms";
 
-class MainPage extends React.Component {
-  render() {
-    return (
-        <div className={styles.MainPage}>
-            <Grid container spacing={0}>
-                <Grid item xs={12}>
-                    <Typography variant='h1'>
-                        In-Sync
-                    </Typography>
-                </Grid>
-                <Grid item sm={12} m={4}>
-                    <Link to={"/login"}>
-                        <Button variant="contained"
-                        size="large" color="primary">
-                        Log In
-                        </Button>
-                    </Link>
-                </Grid>
-                <Grid item xs={12}>
-                    <CreateJoinRoomButton />
-                </Grid>
-            </Grid>
-        </div>
-    );
-  }
-}
+const MainPage = () => {
+  return (
+    <div className={styles.MainPage}>
+      <Grid container spacing={0}>
+        <Grid item xs={12}>
+          <Typography variant='h1'>
+            In-Sync
+          </Typography>
+        </Grid>
+        <Grid item sm={12} m={4}>
+          <Link to={"/login"}>
+            <Button variant="contained"
+              size="large" color="primary">
+              Log In
+            </Button>
+          </Link>
+        </Grid>
+        <Grid item xs={12}>
+          <CreateJoinRoomButton />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
 export default MainPage;

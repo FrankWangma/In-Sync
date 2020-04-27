2219fd539b685a4f1bf4bb6438999c1ede73514b
Frank Wang Ma <frankwangma@gmail.com>
2020-04-27 22:10:20 +1200

    Changed login component to use redux

--- in-sync-frontend/src/App.js
+++ in-sync-frontend/src/App.js
@@ -1,34 +1,15 @@
-import React, { useEffect } from 'react';
+import React from "react";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import { useDispatch, useSelector } from 'react-redux';
 import routes from "./routes/routes";
 
-import { history } from './_helpers';
-import { alertActions } from './_actions';
-
 function App() {
-  const alert = useSelector(state => state.alert);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    history.listen((location, action) => {
-        // clear alert on location change
-        dispatch(alertActions.clear());
-    });
-  }, []);
-
   return (
-    <div>
-      {alert.message &&
-        <div className={`alert ${alert.type}`}>{alert.message}</div>
-      }
-      <Router>
-        <div className="App">
-          {routes}
-        </div>
-      </Router>
-    </div>
+    <Router>
+      <div className="App">
+        {routes}
+      </div>
+    </Router>
   );
 }
 
--- in-sync-frontend/src/components/LoginComponent.js
+++ in-sync-frontend/src/components/LoginComponent.js
@@ -2,29 +2,32 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import styles from "./LoginComponent.module.css"
 import {
-  Card, CardHeader, CardContent,
+  Button, Card, CardHeader, CardContent,TextField, CardActions
 } from "@material-ui/core";
 
 import { userActions } from '../_actions';
 
 const Login = () => {
+  const alert = useSelector(state => state.alert);
   const [inputs, setInputs] = useState({
       username: '',
       password: ''
   });
   const [submitted, setSubmitted] = useState(false);
+  const [helperText, setHelperText] = useState("");
+  const [error, setError] = useState(false);
   const { username, password } = inputs;
   const loggingIn = useSelector(state => state.authentication.loggingIn);
   const dispatch = useDispatch();
 
   // reset login status
   useEffect(() => { 
-      dispatch(userActions.logout()); 
+      dispatch(userActions.logout());
   }, []);
 
   function handleChange(e) {
-      const { name, value } = e.target;
-      setInputs(inputs => ({ ...inputs, [name]: value }));
+      const { id, value } = e.target;
+      setInputs(inputs => ({ ...inputs, [id]: value }));
   }
 
   function handleSubmit(e) {
@@ -36,34 +39,53 @@ const Login = () => {
       }
   }
 
+  function handleAlert() {
+    if(alert.type == "alert-danger") {
+      setError(true);
+      setHelperText(alert.message);
+    }
+  }
+
   return (
       <div className={styles.loginHeader}>
-          <form name="form" onSubmit={handleSubmit}>
+          <form name="form">
           <Card>
-            <CardHeader className={styles.loginHeader} title="Login" />
-            <CardContent>
-              <div className="form-group">
-                  <label>Username</label>
-                  <input type="text" name="username" value={username} onChange={handleChange} className={'form-control' + (submitted && !username ? ' is-invalid' : '')} />
-                  {submitted && !username &&
-                      <div className="invalid-feedback">Username is required</div>
-                  }
-              </div>
-              <div className="form-group">
-                  <label>Password</label>
-                  <input type="password" name="password" value={password} onChange={handleChange} className={'form-control' + (submitted && !password ? ' is-invalid' : '')} />
-                  {submitted && !password &&
-                      <div className="invalid-feedback">Password is required</div>
-                  }
-              </div>
-              <div className="form-group">
-                  <button className="btn btn-primary">
-                      {loggingIn && <span className="spinner-border spinner-border-sm mr-1"></span>}
-                      Login
-                  </button>
-              </div>
-              </CardContent>
-              </Card>
+                <CardHeader className={styles.loginHeader} title="In-Sync Login"/>
+                <CardContent>
+                    <div>
+                        <TextField
+                            error={error}
+                            fullWidth
+                            id="username"
+                            type="text"
+                            label="Username"
+                            placeholder="Username"
+                            margin="normal"
+                            onChange={handleChange}
+                        />
+                        <TextField
+                            error={error}
+                            fullWidth
+                            id="password"
+                            type="password"
+                            label="Password"
+                            placeholder="Password"
+                            margin="normal"
+                            helperText={helperText}
+                            onChange={handleChange}
+                        />
+                    </div>
+                </CardContent>
+                <CardActions>
+                    <Button
+                    variant="contained"
+                    size="large"
+                    color="secondary"
+                    onClick={handleSubmit}>
+                    Login
+                    </Button>
+                </CardActions>
+            </Card>
           </form>
       </div>
   );

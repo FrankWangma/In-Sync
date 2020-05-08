363c3169ed6e25af0e296318bb0d9e69ef456b6f
Hayden <hdnsimpson@gmail.com>
2020-05-08 16:33:00 +1200

    Fixed two click navigation

--- in-sync-frontend/src/components/CreateRoomModal.js
+++ in-sync-frontend/src/components/CreateRoomModal.js
@@ -5,7 +5,7 @@ import {
   Typography,
   Modal,
 } from "@material-ui/core";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import styles from "./Modal.module.css";
 import axios from 'axios';
 
@@ -13,6 +13,7 @@ const CreateRoomModal = ({ showModal, modalHandler }) => {
   const [title, setRoomTitle] = useState("");
   const [url, setURL] = useState("");
   const [roomId, setRoomId] = useState("");
+  const [shouldNavigate, setShouldNavigate] = useState(false);
 
   const createRoom = (title, url) => {
     axios.post("http://localhost:3000/room", {
@@ -20,45 +21,51 @@ const CreateRoomModal = ({ showModal, modalHandler }) => {
       "host": "5ea8de31f4d4b92ac44db792", //PLACEHOLDER
       "video": url,
       "viewers": []
-    }).then(res => setRoomId("/video?id=" + res.data._id));
+    })
+      .then(res => setRoomId("/video?id=" + res.data._id))
+      .then(setShouldNavigate(true));
   }
 
-  return (
-    <Modal open={showModal} onBackdropClick={() => { modalHandler(false); }}>
-      <div className={styles.appModal}>
-        <Typography variant="h2" className={styles.title}>
-          Create Room
+  if (shouldNavigate) {
+    return <Redirect to={roomId} />
+  } else {
+    return (
+      <Modal open={showModal} onBackdropClick={() => { modalHandler(false); }}>
+        <div className={styles.appModal}>
+          <Typography variant="h2" className={styles.title}>
+            Create Room
         </Typography>
-        <Typography>Room Title</Typography>
-        <TextField
-          className={styles.bodyText}
-          InputProps={{ disableUnderline: true }}
-          margin="normal"
-          name="title"
-          value={title}
-          onChange={(e) => { setRoomTitle(e.target.value); }}
-        />
-        <Typography>Video URL</Typography>
-        <TextField
-          className={styles.bodyText}
-          margin="normal"
-          name="url"
-          value={url}
-          onChange={(e) => { setURL(e.target.value); }}
-        />
-        <div className={styles.modalButtons}>
-          <Button className={styles.cancelButton} onClick={() => { modalHandler(false); }}>
-            Cancel
+          <Typography>Room Title</Typography>
+          <TextField
+            className={styles.bodyText}
+            InputProps={{ disableUnderline: true }}
+            margin="normal"
+            name="title"
+            value={title}
+            onChange={(e) => { setRoomTitle(e.target.value); }}
+          />
+          <Typography>Video URL</Typography>
+          <TextField
+            className={styles.bodyText}
+            margin="normal"
+            name="url"
+            value={url}
+            onChange={(e) => { setURL(e.target.value); }}
+          />
+          <div className={styles.modalButtons}>
+            <Button className={styles.cancelButton} onClick={() => { modalHandler(false); }}>
+              Cancel
           </Button>
-          <Link to={roomId} onClick={() => { createRoom(title, url); }}>
-            <Button className={styles.createButton}>
-              Create
+            <Link to={roomId} onClick={() => { createRoom(title, url); }}>
+              <Button className={styles.createButton}>
+                Create
             </Button>
-          </Link>
+            </Link>
+          </div>
         </div>
-      </div>
-    </Modal>
-  );
+      </Modal>
+    );
+  }
 };
 
 export default CreateRoomModal;

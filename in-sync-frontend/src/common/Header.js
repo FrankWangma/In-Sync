a5dbd7e61d8b75fbb76914fed6f8ea27b2fb20bb
Frank Wang Ma <frankwangma@gmail.com>
2020-05-11 11:47:36 +1200

    Added cancel button to header

--- in-sync-frontend/src/common/Header.js
+++ in-sync-frontend/src/common/Header.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
-import { Link } from "react-router-dom";
+import { history } from "../_helpers"
+import { Link, useLocation } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -18,17 +19,28 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const handleCancel = () => {
+  history.goBack();
+}
+
 const Header = () => {
   const classes = useStyles();
+  let location = useLocation();
+  const [isLoginPage, setisLoginPage] = useState(location.pathname === "/login");
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
           <div variant="h6" className={classes.title} />
-          <Link to={"/login"}>
-            <Button variant="contained" color="secondary">Login</Button>
-          </Link>
+          {isLoginPage? 
+            <Button variant="contained" onClick={handleCancel}>
+              Cancel
+            </Button>
+            :
+            <Link to={"/login"}>
+              <Button variant="contained" color="secondary">Login</Button>
+            </Link> }
         </Toolbar>
       </AppBar>
     </div>

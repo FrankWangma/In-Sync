be9b19273406a4f994905fa01997a0b450669e1c
Frank <38555691+FrankWangma@users.noreply.github.com>
2020-05-14 16:17:10 +1200

    Feature persist user information through website (#59)
    * Added cancel button to header
    
    * Added drop down when logged in to replace login button
    
    * Connected logout functionality and Added profile icon image (placeholder)
    
    * Ran automatic lint fix

---
+++ in-sync-frontend/src/common/ProfileButton.js
@@ -0,0 +1,100 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Grow from "@material-ui/core/Grow";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import { makeStyles } from "@material-ui/core/styles";
+import { useDispatch, useSelector } from "react-redux";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import { userActions } from "../_actions";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    color: "white",
+    marginRight: 10,
+  },
+  paper: {
+    marginRight: theme.spacing(2),
+  },
+  button: {
+    textColor: "#ffffff",
+  },
+}));
+
+export default function ProfileButton() {
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const anchorRef = React.useRef(null);
+  const user = useSelector((state) => state.authentication.user);
+  const dispatch = useDispatch();
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  const handleLogout = () => {
+    dispatch(userActions.logout());
+  };
+
+  function handleListKeyDown(event) {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    }
+  }
+
+  // return focus to the button when we transitioned from !open -> open
+  const prevOpen = React.useRef(open);
+  React.useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
+
+  return (
+    <div className={classes.root}>
+      <div>
+        <Button
+          ref={anchorRef}
+          aria-controls={open ? "menu-list-grow" : undefined}
+          aria-haspopup="true"
+          onClick={handleToggle}
+          className={classes.root}
+        >
+            <AccountCircleIcon style={{ marginRight: 10 }}/>
+            {user.username}
+        </Button>
+        <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+          {({ TransitionProps, placement }) => (
+            <Grow
+              {...TransitionProps}
+              style={{ transformOrigin: placement === "bottom" ? "center top" : "center bottom" }}
+            >
+              <Paper>
+                <ClickAwayListener onClickAway={handleClose}>
+                  <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                    <MenuItem onClick={handleClose}>My account</MenuItem>
+                    <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                  </MenuList>
+                </ClickAwayListener>
+              </Paper>
+            </Grow>
+          )}
+        </Popper>
+      </div>
+    </div>
+  );
+}
--- in-sync-frontend/src/pages/LoginPage.js
+++ in-sync-frontend/src/pages/LoginPage.js
@@ -3,19 +3,23 @@ import { Grid } from "@material-ui/core";
 import Login from "../components/LoginComponent";
 import SignUp from "../components/SignUpComponent";
 import styles from "./LoginPage.module.css";
+import Header from "../common/Header";
 
 const LoginPage = () => (
-    <div className={styles.LoginPage}>
-        <Grid container spacing={0}>
-            <Grid item xs={2} />
-            <Grid classes={{ root: styles.grid }} item sm={12} md={4}>
-                <Login />
+    <div>
+        <Header />
+        <div className={styles.LoginPage}>
+            <Grid container spacing={0}>
+                <Grid item xs={2} />
+                <Grid classes={{ root: styles.grid }} item sm={12} md={4}>
+                    <Login />
+                </Grid>
+                <Grid classes={{ root: styles.grid }} item sm={12} md={4}>
+                    <SignUp />
+                </Grid>
+                <Grid item xs={2} />
             </Grid>
-            <Grid classes={{ root: styles.grid }} item sm={12} md={4}>
-                <SignUp />
-            </Grid>
-            <Grid item xs={2} />
-        </Grid>
+        </div>
     </div>
 );
 

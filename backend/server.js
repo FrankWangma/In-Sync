6ba00bb14537e3139249450efa8c43548ef85f94
George McErlean <GeorgeEdwinMcerlean@gmail.com>
2020-05-14 16:11:31 +1200

    Added socket.io setup code

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,14 @@
 import app from './app';
 
-const port = process.env.PORT || '3000'; app.listen(port); 
+const port = process.env.PORT || '3000';
+server = app.listen(port);
+
+const io = require("socket.io")(server);
+
+io.on('connection', (socket) => {
+	console.log('New user connected')
+})
 
 console.log(`Listening on port ${port}`);
+
+export default io;

02c1cd2e7705832ca6f43091596e7c8788ffda6c
Hayden <39291167+hdnsimpson@users.noreply.github.com>
2020-05-14 16:24:41 +1200

    Feature create room functionality (#58)
    * Fixed mainpage comp error
    
    * Getting title and url from modal, added axios
    
    * Create room with correct video url, placeholder host
    
    * Set room id to id in database
    
    * Added room id as url param
    
    * Temp placeholder reminder comment
    
    * Have video page displaying video from url
    
    * Fixed two click navigation
    
    * Lint fixes
    
    * Added cancel button to header
    
    * Added drop down when logged in to replace login button
    
    * Connected logout functionality and Added profile icon image (placeholder)
    
    * Ran automatic lint fix
    
    * Basic join room implementation
    
    * Removed alteration of room id
    
    * Account for vid page user header
    
    Co-authored-by: Frank Wang Ma <frankwangma@gmail.com>

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,6 +2,13 @@ import room from '../controllers/roomController';
 import user from '../controllers/userController';
 
 export default (app) => {
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+    next();
+  });
+
   app.route('/room')
     .get(room.getAllRooms)
     .put(room.joinRoom)

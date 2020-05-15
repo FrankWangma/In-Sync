b684c064cebfae20588a1afc8bc1eeabc5f8247b
Frank Wang Ma <frankwangma@gmail.com>
2020-05-15 22:11:49 +1200

    Added edit functionality to view profile page

--- in-sync-frontend/src/common/ViewProfile.js
+++ in-sync-frontend/src/common/ViewProfile.js
@@ -1,26 +1,57 @@
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   Button,TextField, Card, CardContent, CardHeader, ClickAwayListener, IconButton
 } from "@material-ui/core";
 import EditIcon from '@material-ui/icons/Edit';
+import { userActions } from "../_actions";
 
 
 
 const ViewProfile = (props) => {
+    
+    const currentUser = useSelector((state) => state.authentication.user);
+    const [user, setUser] = useState({
+        firstName: currentUser.firstName,
+        lastName: currentUser.lastName,
+        email: currentUser.email,
+        username: currentUser.userName,
+        password: "",
+      });
     const [viewInfo, setViewInfo] = useState(true);
     const [error, setError] = useState(false);
-    const user = useSelector((state) => state.authentication.user);
+    const [helperText, setHelperText] = useState("");
     const {handleClose} = props;
-    
+    const dispatch = useDispatch();
+
     const handleEdit = () => {
         setViewInfo(false);
-        console.log(user);
     }
 
     const handleSubmit = () => {
-
+        dispatch(userActions.edit(user));
     }
+
+    const handleChange = (e) => {
+        const { id, value } = e.target;
+        if(id === "email") {
+        if(validateEmail(e.target.value)) { 
+            setError(false)
+            setHelperText("");
+        } else {
+            setError(true);
+            setHelperText("Please enter a valid email");
+        }
+        }
+        setUser((inputs) => ({ ...inputs, [id]: value }));
+    }
+
+    function validateEmail(email) {
+        // eslint-disable-next-line no-useless-escape
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+    
 
     return (
         <ClickAwayListener onClickAway={handleClose}>
@@ -43,7 +74,8 @@ const ViewProfile = (props) => {
                         label="First Name"
                         placeholder="First Name"
                         margin="normal" 
-                        defaultValue={user.firstName}
+                        defaultValue={currentUser.firstName}
+                        onChange={handleChange}
                     />
                     <TextField
                         error={error}
@@ -54,7 +86,8 @@ const ViewProfile = (props) => {
                         label="Last Name"
                         placeholder="Last Name"
                         margin="normal"
-                        defaultValue={user.lastName}
+                        defaultValue={currentUser.lastName}
+                        onChange={handleChange}
                     />
                     <TextField 
                         error={error}
@@ -65,7 +98,8 @@ const ViewProfile = (props) => {
                         label="Username"
                         placeholder="Username"
                         margin="normal"
-                        defaultValue={user.username}
+                        defaultValue={currentUser.username}
+                        onChange={handleChange}
                     />  
                     <TextField 
                         error={error}
@@ -76,7 +110,8 @@ const ViewProfile = (props) => {
                         label="Email"
                         placeholder="Email"
                         margin="Email"
-                        defaultValue={user.email}
+                        defaultValue={currentUser.email}
+                        onChange={handleChange}
                     />
                     { viewInfo?
                         null
@@ -90,9 +125,13 @@ const ViewProfile = (props) => {
                             label="Password"
                             placeholder="Password"
                             margin="normal"
+                            helperText={helperText}
+                            onChange={handleChange}
                         />
-                        <Button variant="contained" color="primary">Submit</Button>
-                        <Button variant="outlined">Cancel</Button>
+                        <Button variant="contained" color="primary" onClick={handleSubmit}>
+                            Submit
+                        </Button>
+                        <Button variant="outlined" onClick={handleClose}>Cancel</Button>
                         </div>
                     }
                 </CardContent>

2377f6d034ac3a07947284cba7d1eb6dfc77c4ec
George McErlean <GeorgeEdwinMcerlean@gmail.com>
2020-05-19 23:39:44 +1200

    Updated id generation to be user friendly

--- backend/src/models/Room.js
+++ backend/src/models/Room.js
@@ -1,11 +1,18 @@
 import mongoose, {
   Schema,
 } from 'mongoose';
+import {} from mongo
+
+const nanoid = customAlphabet('1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ', 6)
 
 /**
  * Create database scheme for notes
  */
 const RoomSchema = new Schema({
+  _id: {
+    type: String,
+    default: () => nanoid()
+  },
   host: {
     type: Schema.Types.ObjectId,
     ref: 'User',

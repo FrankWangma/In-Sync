55c010d4154c57e67c4d53aa49f937e6cd0640b3
Frank Wang Ma <frankwangma@gmail.com>
2020-05-19 16:43:32 +1200

    Changed user service to use backend instead of the mock backend

--- in-sync-frontend/src/_services/user.service.js
+++ in-sync-frontend/src/_services/user.service.js
@@ -1,6 +1,7 @@
 import { authHeader } from "../_helpers";
+import axios from "axios";
 
-const apiURL = "http://localhost:4000";
+const apiURL = "http://localhost:3000";
 
 function logout() {
   // remove user from local storage to log user out
@@ -8,49 +9,41 @@ function logout() {
 }
 
 function handleResponse(response) {
-  return response.text().then((text) => {
-    const data = text && JSON.parse(text);
-    if (!response.ok) {
-      if (response.status === 401) {
-        // auto logout if 401 response returned from api
-        logout();
-        window.location.reload(true);
-      }
-
-      const error = (data && data.message) || response.statusText;
-      return Promise.reject(error);
+  if(!response.statusText === "OK") {
+    if (response.status === 401) {
+      // auto logout if 401 response returned from api
+      logout();
+      window.location.reload(true);
     }
+    const error = response.statusText;
+    return Promise.reject(error);
+  }
 
-    return data;
-  });
+  return response.data;
 }
 
 function login(username, password) {
-  const requestOptions = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ username, password }),
-  };
-
-  return fetch(`${apiURL}/users/authenticate`, requestOptions)
-    .then(handleResponse)
+    return axios.post(`${apiURL}/login`, {
+      username: username,
+      password: password,
+    }).then(handleResponse)
     .then((user) => {
       /* store user details and jwt token in local storage
       to keep user logged in between page refreshes */
       localStorage.setItem("user", JSON.stringify(user));
 
       return user;
-    });
+    })
 }
 
 function register(user) {
-  const requestOptions = {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(user),
-  };
-
-  return fetch(`${apiURL}/users/register`, requestOptions).then(handleResponse);
+  return axios.post(`${apiURL}/user/register`,{
+    email: user.email,
+    username: user.username,
+    password: user.password,
+    firstName: user.firstName,
+    lastName:user.lastName
+  }).then(handleResponse);
 }
 
 function update(user) {

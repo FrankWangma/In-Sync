02737b90e97a2a83e4cd86f68ede3571a7f4ad94
George McErlean <GeorgeEdwinMcerlean@gmail.com>
2020-05-24 10:14:06 +1200

    Created tests for mongodb

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -14,6 +14,7 @@ const UserSchema = new Schema({
   username: {
     type: String,
     unique: true,
+    dropDups: true,
     required: true,
   },
   hash: {
@@ -23,6 +24,7 @@ const UserSchema = new Schema({
   email: {
     type: String,
     unique: true,
+    dropDups: true,
     default: '',
   },
 });
@@ -36,6 +38,4 @@ UserSchema.set('toJSON', {
   },
 });
 
-const User = mongoose.model('User', UserSchema);
-
-export default User;
+export default mongoose.model('User', UserSchema);

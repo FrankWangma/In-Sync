d329bfef9c4e884d350378798dd7ab179df0925a
Hayden <39291167+hdnsimpson@users.noreply.github.com>
2020-05-24 22:44:18 +1200

    Feature shadow fix (#101)
    * fixed compilation error
    
    * Added port number console log
    
    * Fixed backend crash
    
    * restore colour to chat user switch

--- backend/src/authentication/errorHandler.js
+++ backend/src/authentication/errorHandler.js
@@ -1,4 +1,4 @@
-function errorHandler(err, req, res) {
+function errorHandler(err, req, res, next) {
   if (err.name === 'UnauthorizedError') {
     // jwt authentication error
     return res.status(401).json({ message: 'Invalid Token' });

9db38344c3ce1fdfb944350074c1ce94f0aa6df8
Frank Wang Ma <frankwangma@gmail.com>
2020-05-24 21:14:27 +1200

    removed the logger middleware

--- in-sync-frontend/src/_helpers/store.js
+++ in-sync-frontend/src/_helpers/store.js
@@ -1,15 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
-import { createLogger } from "redux-logger";
 import rootReducer from "../_reducers";
 
-const loggerMiddleware = createLogger();
 
 // eslint-disable-next-line import/prefer-default-export
 export const store = createStore(
   rootReducer,
   applyMiddleware(
     thunkMiddleware,
-    loggerMiddleware,
   ),
 );
